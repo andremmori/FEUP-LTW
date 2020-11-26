2126b7ebc5e96d9a1595a56309f75dcfc0d93a89
André Mamprin Mori <andremori16@gmail.com>
2020-11-26 18:32:53 +0000

    Implemented function to add user to database

--- pages/database/user.php
+++ pages/database/user.php
@@ -1,8 +1,49 @@
 <?php
     function getAllUsers() {
+        global $db;
         $stmt = $db->prepare('SELECT * FROM user');
         $stmt->execute();
         return $stmt->fetchAll();
+    }
+
+    // Add User to the database
+    function addUser(){
+        global $db;
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+        $username = $_GET['username'];
+        $password = $_GET['password'];
+        $repeat = $_GET['repeat'];
+
+        if($password != $repeat) return false;
+
+        try {
+            // Init transaction
+            $db->beginTransaction();
+
+            // Insert Account
+            $account_sql = "INSERT INTO account (name) VALUES (?)";
+            $first_stmt = $db->prepare($account_sql);
+            $first_exec = $first_stmt->execute([$name]);
+
+            if(!$exec) throw new Exception();
+
+            // Get Account id after insert
+            $account_id = $db->lastInsertId();
+
+            // Insert User
+            $user_sql = "INSERT INTO user (email, passwordhash) VALUES (?, ?)";
+            $second_stmt = $db->prepare($user_sql);
+            $second_exec = $second_stmt->execute([$email, $password]);
+
+            if(!$exec) throw new Exception();
+
+            $db->commit();
+        } catch (Exception $e) {
+            $db->rollback();
+            return false;
+        }
+        return true;
     }
 
 ?>
