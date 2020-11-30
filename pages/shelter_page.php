5c55e5efd8adca94a9da00ae48e1f28c9b1e5f0a
André Mamprin Mori <andremori16@gmail.com>
2020-11-30 16:59:25 +0000

    Started implementing Shelter page

---
+++ pages/shelter_page.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Petgram</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/home.css" rel="stylesheet">
+    <link rel="icon" href="images/icon.jpg">
+</head>
+
+<body>
+    <div id="hotBar">
+        <h1>Petgram</h1>
+        <a href=""><img src="images/home.png" alt="" width="50" height="50"></a>
+        <a href="register.php"><img src="images/profile.png" alt="" width="50" height="50"></a>
+        <a href=""><img src="images/search.png" alt="" width="50" height="50"></a>
+        <a href=""><img src="images/settings.png" alt="" width="50" height="50"></a>
+        <a href=""><img src="images/logout.png" alt="" width="50" height="50"></a>
+    </div>
+    <aside id="sideBar">
+        <div id="user">
+            <img src="images/pfp.png" alt="" width="65" height="65">
+            <div id="username">
+                <p>Average pet fan</p>
+                <p>petfan123</p>
+                <p>Collaborator of Good Boy Shelter</p>
+            </div>
+        </div>
+        <p>Pets:</p>
+        <ul>
+            <li>Animal 1</li>
+            <li>Animal 2</li>
+            <li>Animal 3</li>
+        </ul>
+        <img src="images/petAdd.png" alt="" width="35" height="35">
+    </aside>
+    <section id="profile">
+        <div id="top">
+            <img id="edit" src="images/edit.png" alt="" width="35" height="35">
+            <img id="petpic" src="images/shelter_profile.jpeg" alt="" width="65" height="65">
+            <h1 id="name">Good Boy Shelter</h1>
+            <p id="followers">Followers 151</p>
+            <p id="following">Following 4</p>
+            <p id="bio">Sou um shelter muito bonito com varios animais fofinhos hihihi</p>
+        </div>
+        <div id="listing">
+            <p>Estes são nossos good boys para adoção.</p>
+        </div>
+        <div id="gallery">
+            <div class="row">
+                <img src="images/puppy.jpg" alt="" width="65" height="65">
+                <img src="images/puppy2.jpeg" alt="" width="65" height="65">
+                <img src="images/puppy3.jpeg" alt="" width="65" height="65">
+            </div>
+            <div class="row">
+                <img src="images/puppy4.jpeg" alt="" width="65" height="65">
+            </div>
+        </div>
+    </section>
+    <footer>
+        <p>&copy; Petgram 2020</p>
+    </footer>
+</body>
+
+</html>
