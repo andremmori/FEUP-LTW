589d4f0a3a1c982964f86d765775532a976a7830
André Mamprin Mori <andremori16@gmail.com>
2020-12-05 16:26:54 +0000

    Added modal to login in main_page.php

--- pages/main_page.php
+++ pages/main_page.php
@@ -1,31 +1,38 @@
+<?php
+include_once('database/connection.php');
+?>
 <!DOCTYPE html>
 <html lang="en-US">
-  <head>
+
+<head>
     <title>Petgram</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/main_mage.css" rel="stylesheet">
+    <link href="css/home.css" rel="stylesheet">
     <link rel="icon" href="images/icon.jpg">
-  </head>
-  <body>
+</head>
+
+<body>
+    <?php include_once('header.php') ?>
     <header>
-      <h1>Petgram</h1>
-      <h2>The best pet social media for adoption!</h2>
+        <h1>Petgram</h1>
+        <h2>The best pet social media for adoption!</h2>
     </header>
     <div id="login">
-      <form action="login_action.php" method="post">
-        <label>Email: <input type="email" name="email" required></label> <br>
-        <label>Password: <input type="password" name="password" required></label> <br>
-        <input type="submit" value="Login">
-      </form>
-      <a href="home.php"><button>Skip to Home Screen</button></a>
+        <form action="login_action.php" method="post">
+            <label>Email: <input type="email" name="email" required></label> <br>
+            <label>Password: <input type="password" name="password" required></label> <br>
+            <input type="submit" value="Login">
+        </form>
+        <a href="home.php"><button>Skip to Home Screen</button></a>
     </div>
     <div id="register">
-      <p>Don't have an account? Register now!</p>
-      <a href="register.php"><button>Register</button></a>
+        <p>Don't have an account? Register now!</p>
+        <a href="register.php"><button>Register</button></a>
     </div>
     <footer>
-      <p>&copy; Petgram 2020</p>
+        <p>&copy; Petgram 2020</p>
     </footer>
-  </body>
+</body>
+
 </html>
---
+++ pages/modal/login.php
@@ -0,0 +1,25 @@
+<div id="login-modal" class="modal">
+    <form class="modal-content animate" action="/login_action.php" method="post">
+        <div class="imgcontainer">
+            <span onclick="document.getElementById('login-modal').style.display='none'" class="close" title="Close Modal">&times;</span>
+            <!-- <img src="img_avatar2.png" alt="Avatar" class="avatar"> -->
+            <h1>Petgram</h1>
+        </div>
+        <br>
+        <div class="container">
+                <label for="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" name="email" required>
+                <br>
+                <br>
+                <label for="password"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="password" required>
+                <br>
+                <br>
+                <button id="btn" type="submit">Login</button>
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+            <button type="button" onclick="document.getElementById('login-modal').style.display='none'" class="cancelbtn">Cancel</button>
+        </div>
+    </form>
+</div>
