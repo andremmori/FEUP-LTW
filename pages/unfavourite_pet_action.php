861ea3c836dbf9bdd56416e48fb5408b6130a6c2
André Mamprin Mori <andremori16@gmail.com>
2020-12-18 01:47:44 +0000

    Updated pet profile followers display

--- pages/unfavourite_pet_action.php
+++ pages/unfavourite_pet_action.php
@@ -2,4 +2,4 @@
 include_once('database/connection.php');
 include_once('database/favourite.php');
 
-removeFromFavourites($_GET['account_id'], $_GET['pet_id']);
+echo removeFromFavourites($_GET['account_id'], $_GET['pet_id']);
